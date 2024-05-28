2f9e3e70b6340ec68ff45f544683bb4b8cc060b3
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-05-28 23:56:56 +0300

    Çalışmalar eklendi.

--- Example06.WebApplication/Controllers/HotelController.cs
+++ Example06.WebApplication/Controllers/HotelController.cs
@@ -9,27 +9,34 @@ namespace Example06.WebApplication.Controllers
     public class HotelController : Controller
     {
         private AppDbContext _appDbContext;
-        public HotelController()
+        private ILogger<HotelController> _logger;
+        public HotelController(AppDbContext appDbContext, ILogger<HotelController> logger)
         {
-            _appDbContext = new AppDbContext(); 
+            _appDbContext = appDbContext;
+            _logger=logger;
         }
 
         // GET: HotelController
         public ActionResult Index()
         {
             var oteller=_appDbContext.Otels.ToList();
-
+            _logger.LogInformation("Oteller sayfası listeleniyor toplam otel sayısı: " + oteller.Count);
+            _logger.LogError("Oteller sayfası listeleniyor toplam otel sayısı: " + oteller.Count);
+            _logger.LogWarning("Oteller sayfası listeleniyor toplam otel sayısı: " + oteller.Count);
+            _logger.LogCritical("Oteller sayfası listeleniyor toplam otel sayısı: " + oteller.Count);
             return View(oteller);
         }
 
         // GET: HotelController/Details/5
         public ActionResult Details(int id)
         {
+            Otel model = _appDbContext.Otels.Single(o => o.Id == id);
+
             Hotel hotel = new Hotel();
-            hotel.Id = id;
-            hotel.Name = "Otel adı" + id;
-            hotel.Description = "Otel açıklama" + id;
-            hotel.Phone = "123456789";
+            hotel.Id = model.Id;
+            hotel.Name = model.Name;
+            hotel.Description = model.Description;
+            hotel.Phone = model.Phone;
             return View(hotel);
         }
 
@@ -60,16 +67,26 @@ namespace Example06.WebApplication.Controllers
         // GET: HotelController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Otel model = _appDbContext.Otels.Single(o => o.Id == id);
+
+            return View(model);
         }
 
         // POST: HotelController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Otel newmodel)
         {
             try
             {
+                Otel oldModel = _appDbContext.Otels.Single(o => o.Id == id);
+                oldModel.Name= newmodel.Name;
+                oldModel.Description= newmodel.Description;
+                oldModel.Phone= newmodel.Phone;
+
+                _appDbContext.Otels.Update(oldModel);
+                _appDbContext.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -81,16 +98,39 @@ namespace Example06.WebApplication.Controllers
         // GET: HotelController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Otel Model = _appDbContext.Otels.Single(o => o.Id == id);
+
+            return View(Model);
+        }
+
+   
+        public ActionResult DeleteConfirm2(int id)
+        {
+            try
+            {
+                Otel Model = _appDbContext.Otels.Single(o => o.Id == id);
+                _appDbContext.Otels.Remove(Model);
+                _appDbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // POST: HotelController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteConfirm(int id)
         {
             try
             {
+                Otel Model=_appDbContext.Otels.Single(o => o.Id == id);
+                _appDbContext.Otels.Remove(Model);
+                _appDbContext.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
--- Example06.WebApplication/Program.cs
+++ Example06.WebApplication/Program.cs
@@ -1,6 +1,9 @@
+using Example06.WebApplication.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
