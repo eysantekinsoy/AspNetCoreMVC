c3772c75db92a0d29d344bcbf8504a63a9134b6a
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-06-02 00:28:50 +0300

    File Upload

---
+++ Example07.WebApplication/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Example07.WebApplication.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example07.WebApplication.Data
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Example07.WebApplication.Data.Entities.Hotel> Hotel { get; set; } = default!;
+    }
+}
---
+++ Example07.WebApplication/Data/Entities/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace Example07.WebApplication.Data.Entities
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LogoFile { get; set; }
+
+    }
+}
